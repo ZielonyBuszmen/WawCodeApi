1e3a09caead0aa2b092d4e8b0a192eb7bb6b0d20
Krzysztof Komar <krzysztof.komar@gazeta.pl>
2017-10-15 11:44:20 +0200

    Encja chatu

---
+++ src/AppBundle/Entity/Chat.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="historical_events")
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\Repository\ChatRepository")
+ */
+class Chat
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    public function getID()
+    {
+        return $this->id;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTime $date)
+    {
+        $this->date = $date;
+    }
+
+}
---
+++ src/AppBundle/Entity/Repository/ChatRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace AppBundle\Entity\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class ChatRepository extends EntityRepository
+{
+    public function findLastTen()
+    {
+
+    }
+}
